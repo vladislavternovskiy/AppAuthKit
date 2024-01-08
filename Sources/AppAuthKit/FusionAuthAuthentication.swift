1a21d6916b05843444abc70aefdf964f21d78506
Vladyslav Ternovskyi <vladislav.ternovskii@gmail.com>
2024-01-08 17:29:14 +0200

    Update FusionAuthAuthentication.swift

--- Sources/AppAuthKit/FusionAuthAuthentication.swift
+++ Sources/AppAuthKit/FusionAuthAuthentication.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-struct FusionAuthAuthentication: Authentication {
+public struct FusionAuthAuthentication: Authentication {
 
-    let clientId: String
-    let url: URL
+    public let clientId: String
+    public let url: URL
 
     let session: URLSession
 
-    init(clientId: String, url: URL, session: URLSession = URLSession.shared) {
+    public init(clientId: String, url: URL, session: URLSession = URLSession.shared) {
         self.clientId = clientId
         self.url = url
         self.session = session
     }
 
-    func login(email: String, code: String) -> FusionRequest<Credentials, AuthenticationError> {
+    public func login(email: String, code: String) -> FusionRequest<Credentials, AuthenticationError> {
         return login(username: email, otp: code)
     }
 
-    func login(usernameOrEmail username: String, password: String) -> FusionRequest<Credentials, AuthenticationError> {
+    public func login(usernameOrEmail username: String, password: String) -> FusionRequest<Credentials, AuthenticationError> {
         let url = URL(string: "/api/login", relativeTo: self.url)!
         let payload: [String: Any] = [
             "loginId": username,
@@ -38,7 +38,7 @@ struct FusionAuthAuthentication: Authentication {
                        parameters: payload)
     }
     
-    func forgotPassword(email: String) -> FusionRequest<Void, AuthenticationError> {
+    public func forgotPassword(email: String) -> FusionRequest<Void, AuthenticationError> {
         let payload: [String: Any] = [
             "loginId": email,
             "applicationId": clientId,
@@ -52,7 +52,7 @@ struct FusionAuthAuthentication: Authentication {
                        parameters: payload)
     }
 
-    func renew(withRefreshToken refreshToken: String) -> FusionRequest<Credentials, AuthenticationError> {
+    public func renew(withRefreshToken refreshToken: String) -> FusionRequest<Credentials, AuthenticationError> {
         let payload: [String: Any] = [
             "client_id": clientId,
             "refresh_token": refreshToken,
@@ -68,7 +68,7 @@ struct FusionAuthAuthentication: Authentication {
         )
     }
 
-    func revoke(refreshToken: String) -> FusionRequest<Void, AuthenticationError> {
+    public func revoke(refreshToken: String) -> FusionRequest<Void, AuthenticationError> {
         let payload: [String: Any] = [
             "refresh_token": refreshToken,
             "global": true
