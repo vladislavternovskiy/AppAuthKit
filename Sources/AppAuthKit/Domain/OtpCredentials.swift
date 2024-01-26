b2da827f2c9215ad47c10a157b4b4aed5577b67b
Vladyslav Ternovskyi <vladislav.ternovskii@gmail.com>
2024-01-26 16:09:11 +0200

    add jwt token decoder

--- Sources/AppAuthKit/Domain/OtpCredentials.swift
+++ Sources/AppAuthKit/Domain/OtpCredentials.swift
@@ -14,4 +14,17 @@ public final class OtpCredentials: Codable {
     public func mapToCredentials() -> Credentials {
         .init(otpCredentials: self)
     }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        token = try values.decode(String.self, forKey: .token)
+        refreshToken = try? values.decode(String.self, forKey: .refreshToken)
+        if let exp = try? values.decode(Date.self, forKey: .tokenExpirationInstant) {
+            tokenExpirationInstant = exp
+        } else if let jwt = try? decode(jwt: token) {
+            tokenExpirationInstant = jwt.expiresAt
+        } else {
+            tokenExpirationInstant = nil
+        }
+    }
 }
