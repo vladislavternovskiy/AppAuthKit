21925f241758651c2e02272fdf859a220d63626a
Vladyslav Ternovskyi <vladislav.ternovskii@gmail.com>
2025-06-23 17:04:56 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AppAuthKit",
-    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7)],
+    platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v13), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
